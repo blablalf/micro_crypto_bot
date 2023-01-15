2ed789d0d5d263eaf972cb2e2fc1ac53f7a55a96
Alfred Gaillard <alfred.gaillard@smart-chain.fr>
2023-01-15 23:53:22 +0100

    Mark resolution

--- front/src/Average.js
+++ front/src/Average.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Average() {
+
+  const pair = 'ETH-USD';
   const [data, setData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const result = await axios.get('http://127.0.0.1:3000/average/ETH-USD');
+        const result = await axios.get('http://127.0.0.1:3000/average/' + pair);
         setData(result.data);
       } catch (error) {
         console.log(error);
@@ -18,6 +20,7 @@ function Average() {
 
   return (
     <div>
+      <p>Average price of {pair} :</p>
       {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </div>
   );
--- front/src/LastDayPrices.js
+++ front/src/LastDayPrices.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function LastDayPrices() {
+
+  const pair = 'ETH-USD';
   const [data, setData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const result = await axios.get('http://127.0.0.1:3000/prices/ETH-USD');
+        const result = await axios.get('http://127.0.0.1:3000/prices/' + pair);
         setData(result.data);
       } catch (error) {
         console.log(error);
@@ -18,6 +20,7 @@ function LastDayPrices() {
 
   return (
     <div>
+      <p>Last 24 hours prices of {pair} :</p>
       {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </div>
   );
